ca61a4f482ea392c697f8bef7fab87b26e38bcf7
Chinonso Wisdom <nonsowisdom62@gmail.com>
2023-08-10 12:54:58 +0100

    Finished

--- main.js
+++ main.js
@@ -1,63 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var animationContainer = document.querySelector(".animation-container");
-    var pageContents = document.querySelector(".page-contents");
-    
-    setTimeout(function () {
+$(document).ready(() => {
+    const animationContainer = $(".animation-container");
+    const pageContents = $(".page-contents");
+    const scrollToTopButton = $(".scroll-to-top");
+    const headerText = $(".header-text");
+  
+    setTimeout(() => {
       animationContainer.remove();
-      pageContents.style.visibility = "visible";
+      pageContents.css("visibility", "visible");
     }, 5000);
-  });
-
-$(document).ready(function () {
-    // $(".hover-dropdown").on("mouseenter click", function () {
-    //   var dropdownMenu = $(this).find(".dropdown-menu");
-    //   dropdownMenu.toggle();
-    // });
-    $(".hover-dropdown .dropdown-menu").on("click", function (e) {
-        e.stopPropagation();
-        });
-    // $(document).on("click", function (e) {
-    //   if (!$(".hover-dropdown").is(e.target) && $(".hover-dropdown").has(e.target).length === 0) {
-    //     $(".dropdown-menu").hide();
-    //   }
-    // });
-
-    let lastScroll = 0;
-    $(window).on("scroll", function () {
-        var currentScroll = $(this).scrollTop();
-
-        if (currentScroll > lastScroll) {
-          $(".navbar").addClass("hidden-navbar");
-        } else {
-          $(".navbar").removeClass("hidden-navbar");
-        }
-
-        lastScroll = currentScroll;
-      });
-
-
-      let scrollToTopButton = document.querySelector(".scroll-to-top");
-    scrollToTopButton.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  
+    $(".hover-dropdown .dropdown-menu").on("click", (e) => {
+      e.stopPropagation();
     });
-    window.addEventListener("scroll", function () {
-    if (window.scrollY > 300) {
-        scrollToTopButton.classList.add("active");
-    } else {
-        scrollToTopButton.classList.remove("active");
-    }
-});
+  
+    let lastScroll = 0;
+    $(window).on("scroll", () => {
+      const currentScroll = $(window).scrollTop();
+  
+      if (currentScroll > lastScroll) {
+        $(".navbar").addClass("hidden-navbar");
+      } else {
+        $(".navbar").removeClass("hidden-navbar");
+      }
+  
+      lastScroll = currentScroll;
+    });
+  
+    scrollToTopButton.on("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  
+    $(window).on("scroll", () => {
+      if (window.scrollY > 300) {
+        scrollToTopButton.addClass("active");
+      } else {
+        scrollToTopButton.removeClass("active");
+      }
+    });
+  
+    $(window).on("load", () => {
+      headerText.addClass("header-text-visible");
+    });
   });
-
-var scrollToTopButton = document.querySelector(".scroll-to-top");
-scrollToTopButton.addEventListener("click", function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 300) {
-    scrollToTopButton.classList.add("active");
-  } else {
-    scrollToTopButton.classList.remove("active");
-  }
-});
-
+  
