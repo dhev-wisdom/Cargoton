c6176ec1ca78794dc7a77519797a6acd82c02ed4
Chinonso Wisdom <nonsowisdom62@gmail.com>
2023-08-10 02:18:07 +0100

    Finished

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
-// Prevent dropdown from closing when clicking on the dropdown menu
-$(".hover-dropdown .dropdown-menu").on("click", function (e) {
-    e.stopPropagation();
+document.addEventListener("DOMContentLoaded", function () {
+    var animationContainer = document.querySelector(".animation-container");
+    var pageContents = document.querySelector(".page-contents");
+    
+    setTimeout(function () {
+      animationContainer.remove();
+      pageContents.style.visibility = "visible";
+    }, 5000); // 5 seconds delay
   });
-  
+
+$(document).ready(function () {
+    // $(".hover-dropdown").on("mouseenter click", function () {
+    //   var dropdownMenu = $(this).find(".dropdown-menu");
+    //   dropdownMenu.toggle();
+    // });
+    $(".hover-dropdown .dropdown-menu").on("click", function (e) {
+        e.stopPropagation();
+        });
+    // $(document).on("click", function (e) {
+    //   if (!$(".hover-dropdown").is(e.target) && $(".hover-dropdown").has(e.target).length === 0) {
+    //     $(".dropdown-menu").hide();
+    //   }
+    // });
+
+    let lastScroll = 0;
+    $(window).on("scroll", function () {
+        var currentScroll = $(this).scrollTop();
+
+        if (currentScroll > lastScroll) {
+          $(".navbar").addClass("hidden-navbar");
+        } else {
+          $(".navbar").removeClass("hidden-navbar");
+        }
+
+        lastScroll = currentScroll;
+      });
+  });
+
+  // Scroll to top button
+var scrollToTopButton = document.querySelector(".scroll-to-top");
+scrollToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    scrollToTopButton.classList.add("active");
+  } else {
+    scrollToTopButton.classList.remove("active");
+  }
+});
+
